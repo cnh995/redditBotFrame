5648b4b49ac5b86ca9b07846d022ab75f408abe4
cnh995 <c_hansen_5@hotmail.com>
2016-02-16 00:54:55 -0600

    postsearch.py
    Adds functionality to search post titles for keywords, copy into bot.py to work.

---
+++ postsearch.py
@@ -0,0 +1,15 @@
+#PASTE INTO BOT.PY
+
+
+
+    submissions = subreddit.get_hot(limit = 50) #can change to find new post, hot posts, controversial, etc
+    keywords = [''] #looks for keywords in post title
+    for submission in submissions:
+        post_text = submission.title.lower()
+        postMatch = any(string in post_text for string in keywords) #checks for match between post title and keywords
+        if postMatch and submission.id not in postsRepliedTo:
+            submission.add_comment('') #enter comment text in quotes
+            postsRepliedTo.append(submission.id)
+            with open("postsRepliedTo.txt", "w") as f:
+                for post_id in postsRepliedTo:
+                    f.write(post_id + "\n")
