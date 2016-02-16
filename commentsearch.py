fb226c0bcf7b6eead5eaeee2e6a918842725ad04
cnh995 <c_hansen_5@hotmail.com>
2016-02-16 01:07:07 -0600

    Update commentsearch.py

--- commentsearch.py
+++ commentsearch.py
@@ -11,12 +11,13 @@ else:
 
 #PASTE INTO FUNCTION AREA OF BOT.PY
     comments = subreddit.get_comments(limit = 50)  #searches comments
+    keywords = [''] #sets up keywords to look for
     for comment in comments:
         comment_text = comment.body.lower()
-        isMatch = any(string in comment_text for string in words) #searches for matches between comment body and keywords list
-        if comment.id not in commentsRepliedTo and isMatch:
+        commentMatch = any(string in comment_text for string in keywords) #searches for matches between comment body and keywords list
+        if commentMatch and comment.id not in commentsRepliedTo:
             comment.reply('') #enter comment reply in quotes
-            cache.append(comment.id)
+            commentsRepliedTo.append(comment.id)
             with open("commentsRepliedTo.txt", "w") as f: #adds comment id to a list, so it will never be replied to again
                 for comment_id in commentsRepliedTo:
                     f.write(comment_id + "\n")
